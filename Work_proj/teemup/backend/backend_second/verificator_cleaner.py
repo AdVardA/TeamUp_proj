a0dfed3f221be368234c29b60c0a9aa7117b4bd0
Art <Art@gmail.com>
2024-05-03 23:05:35 +0300

    commit

---
+++ Work_proj/teemup/backend/backend_second/verificator_cleaner.py
@@ -0,0 +1,9 @@
+"""cleaner"""
+from django.contrib.auth.models import User
+
+from .models import Profile, Unverified_Profile, Verificator, Univer_Profile, Club_Profile, Open_Position_for_Un
+from .models import Achivment, Extra_Languages, Teem
+
+
+def verificator_clean():
+    pass
